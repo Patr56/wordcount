784dc55073f298c6124f20ed1cc0c1b7813ff541
sbt-khoroshilov-ps <pskhoroshilov.sbt@sberbank.ru>
2023-01-28 17:38:59 +0300

    fix test failed

--- main.go
+++ main.go
@@ -8,7 +8,18 @@ import (
 
 func main() {
 	args := os.Args
+
+	if args[1] == "" {
+		fmt.Println(0)
+		return
+	}
+
 	words := strings.Split(args[1], " ")
 
-	fmt.Println(len(words))
+	if args[1] == words[0] {
+		fmt.Println(1)
+	} else {
+		fmt.Println(len(words))
+	}
+
 }
